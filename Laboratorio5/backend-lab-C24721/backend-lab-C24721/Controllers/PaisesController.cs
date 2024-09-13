d9df19bc06ff73f13586678043ba0033a45d761c
FelipeMena11 <felipe.menarodriguez@ucr.ac.cr>
2024-09-13 15:45:27 -0600

    Added Lab 5 folder to repository

---
+++ Laboratorio5/backend-lab-C24721/backend-lab-C24721/Controllers/PaisesController.cs
@@ -0,0 +1,25 @@
+﻿using backend_lab.Handlers;
+using backend_lab_C24721.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_lab_C24721.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
+    {
+        private readonly PaisesHandler _paisesHander;
+
+        public PaisesController()
+        {
+            _paisesHander = new PaisesHandler();
+        }
+        [HttpGet]
+        public List<PaisModel> Get()
+        {
+            var paises = _paisesHander.ObtenerPaises();
+            return paises;
+        }
+    }
+}
